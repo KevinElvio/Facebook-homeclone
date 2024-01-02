c1379a1be5ee66808ec4cceb8b8d49e345b042b3
haris ardian <harisfirman387@gmail.com>
2024-01-02 12:18:20 +0700

    feat navbar dan status

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -27,25 +29,100 @@ class _HomepageState extends State<Homepage> {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(100.0),
-        child: AppBar(
+        appBar: AppBar(
+          toolbarHeight: 100,
           backgroundColor: Color.fromARGB(255, 255, 255, 255),
           shadowColor: Color.fromARGB(255, 241, 238, 238),
-          title: Text(
-            'Facebook',
-            style: TextStyle(
-                color: Colors.blue.shade900, fontWeight: FontWeight.w800),
+          elevation: 0,
+          title: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 2,
+                    child: Text(
+                      'Facebook',
+                      style: TextStyle(
+                          color: Colors.blue.shade900,
+                          fontWeight: FontWeight.w900,
+                          fontSize: 24),
+                    ),
+                  ),
+                  const Icon(
+                    Icons.search,
+                    weight: 50,
+                  ),
+                  const Icon(
+                    Icons.circle,
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Icon(
+                    Icons.home,
+                    size: 40,
+                  ),
+                  Icon(
+                    Icons.forum_outlined,
+                    size: 40,
+                  ),
+                  Icon(
+                    Icons.notifications,
+                    size: 40,
+                  ),
+                  Icon(
+                    Icons.widgets,
+                    size: 40,
+                  ),
+                ],
+              )
+            ],
           ),
+          iconTheme: IconThemeData(color: Colors.blue.shade900),
         ),
-      ),
-      body: Center(
-        child: Text(
-          'Content for selected index: $_currentIndex',
-          style: TextStyle(fontSize: 18),
-        ),
-      ),
-    );
+        body: Container(
+          color: Color.fromARGB(255, 221, 221, 221),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    alignment: Alignment.center,
+                    width: screenWidth,
+                    margin: EdgeInsets.only(top: 20),
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Container(
+                          width: 90.0,
+                          height: 130.0,
+                          decoration: BoxDecoration(
+                              shape: BoxShape
+                                  .rectangle, // Mengatur bentuk kotak menjadi lingkaran
+                              color: Color.fromARGB(255, 180, 180, 180),
+                              borderRadius: BorderRadius.circular(10)),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Text('test')
+            ],
+          ),
+        ));
   }
 }
