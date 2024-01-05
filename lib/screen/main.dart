2444b0862cab00cc2a6d2e3dd2cace53ffd4edad
haris ardian <harisfirman387@gmail.com>
2024-01-05 18:42:47 +0700

    update post ui

--- lib/screen/main.dart
+++ lib/screen/main.dart
@@ -88,12 +88,12 @@ class _HomepageState extends State<Homepage> {
       ),
       body: SingleChildScrollView(
         child: Container(
-          color: Color.fromARGB(255, 145, 145, 145),
+          color: Color.fromARGB(255, 206, 203, 203),
           child: Column(
             children: [
               Container(
                 color: const Color.fromARGB(255, 255, 255, 255),
-                margin: EdgeInsets.only(top: 5),
+                margin: EdgeInsets.only(top: 2),
                 padding: EdgeInsets.all(5),
                 child: CarouselSlider(
                   options: CarouselOptions(
@@ -122,7 +122,7 @@ class _HomepageState extends State<Homepage> {
                   Container(
                     width: screenWidth,
                     color: const Color.fromARGB(255, 255, 255, 255),
-                    margin: EdgeInsets.only(top: 5),
+                    margin: EdgeInsets.only(top: 2),
                     child: Column(
                       children: [
                         Container(
@@ -131,7 +131,7 @@ class _HomepageState extends State<Homepage> {
                           child: Row(
                             children: [
                               Container(
-                                  width: 70,
+                                  width: 60,
                                   color: Color.fromARGB(255, 250, 250, 250),
                                   child: const Row(
                                     mainAxisAlignment: MainAxisAlignment.end,
@@ -152,7 +152,7 @@ class _HomepageState extends State<Homepage> {
                                     Text(
                                       "Oniel",
                                       style: TextStyle(
-                                          fontSize: 20,
+                                          fontSize: 18,
                                           color:
                                               Color.fromARGB(255, 33, 33, 34),
                                           fontWeight: FontWeight.w600),
@@ -171,17 +171,241 @@ class _HomepageState extends State<Homepage> {
                           ),
                         ),
                         Container(
-                          margin: EdgeInsets.only(left: 30, bottom: 5),
+                          margin: EdgeInsets.only(left: 20, bottom: 5),
                           alignment: Alignment.centerLeft,
-                          child: Text('Hari ini RKJ seruuuu!!',
+                          child: const Text(
+                              'mengawali tahun dengan optimis, mengakhiri tahun dengan memaafkan<3',
                               style: TextStyle(
                                   fontSize: 15, fontWeight: FontWeight.w500)),
                         ),
                         Container(
                           height: 500,
                           width: screenWidth,
-                          color: const Color.fromARGB(255, 129, 129, 129),
+                          decoration: const BoxDecoration(
+                              border: Border(
+                                  bottom: BorderSide(
+                            color: Color.fromARGB(66, 168, 168, 168),
+                            width: 1,
+                          ))),
+                          child:
+                              Image.asset('assets/images/GCplz0ybMAAvjUh.jpg'),
                         ),
+                        Container(
+                          width: screenWidth,
+                          margin: const EdgeInsets.only(top: 20, bottom: 20),
+                          padding: EdgeInsets.all(5),
+                          decoration: const BoxDecoration(
+                              border: Border(
+                            top: BorderSide(
+                              color: Color.fromARGB(66, 168, 168, 168),
+                              width: 1,
+                            ),
+                          )),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                  width: 100,
+                                  child: const Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      Icon(
+                                        Icons.thumb_up_outlined,
+                                        size: 30,
+                                      ),
+                                      Text("Suka")
+                                    ],
+                                  )),
+                              Container(
+                                  width: 100,
+                                  child: const Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      Icon(
+                                        Icons.comment_outlined,
+                                        size: 30,
+                                      ),
+                                      Text("Komentar")
+                                    ],
+                                  )),
+                              Container(
+                                  width: 100,
+                                  child: const Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      Icon(
+                                        Icons.share,
+                                        size: 30,
+                                      ),
+                                      Text("Bagikan")
+                                    ],
+                                  )),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                margin: EdgeInsets.only(top: 2),
+                padding: EdgeInsets.all(5),
+                child: CarouselSlider(
+                  options: CarouselOptions(
+                    height: 140.0,
+                    enableInfiniteScroll: false,
+                    aspectRatio: 16 / 9,
+                    viewportFraction: 0.5,
+                  ),
+                  items: [1, 2, 3, 4, 5].map((i) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return Container(
+                          width: screenWidth * 0.8,
+                          margin: EdgeInsets.symmetric(horizontal: 5.0),
+                          decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 184, 184, 184)),
+                        );
+                      },
+                    );
+                  }).toList(),
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: screenWidth,
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                    margin: EdgeInsets.only(top: 2),
+                    child: Column(
+                      children: [
+                        Container(
+                          width: screenWidth,
+                          padding: EdgeInsets.all(5),
+                          child: Row(
+                            children: [
+                              Container(
+                                  width: 60,
+                                  color: Color.fromARGB(255, 250, 250, 250),
+                                  child: const Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      Icon(
+                                        Icons.circle,
+                                        size: 50,
+                                      ),
+                                    ],
+                                  )),
+                              Container(
+                                margin: EdgeInsets.only(left: 10),
+                                color: Color.fromARGB(255, 255, 255, 255),
+                                child: const Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Oniel",
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          color:
+                                              Color.fromARGB(255, 33, 33, 34),
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                    Text(
+                                      '12 jam lalu',
+                                      style: TextStyle(
+                                          color:
+                                              Color.fromARGB(255, 97, 97, 102),
+                                          fontWeight: FontWeight.w100),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: 20, bottom: 5),
+                          alignment: Alignment.centerLeft,
+                          child: const Text(
+                              'mengawali tahun dengan optimis, mengakhiri tahun dengan memaafkan<3',
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.w500)),
+                        ),
+                        Container(
+                          height: 500,
+                          width: screenWidth,
+                          decoration: const BoxDecoration(
+                              border: Border(
+                                  bottom: BorderSide(
+                            color: Color.fromARGB(66, 168, 168, 168),
+                            width: 1,
+                          ))),
+                          child:
+                              Image.asset('assets/images/GCplz0ybMAAvjUh.jpg'),
+                        ),
+                        Container(
+                          width: screenWidth,
+                          margin: const EdgeInsets.only(top: 20, bottom: 20),
+                          padding: EdgeInsets.all(5),
+                          decoration: const BoxDecoration(
+                              border: Border(
+                            top: BorderSide(
+                              color: Color.fromARGB(66, 168, 168, 168),
+                              width: 1,
+                            ),
+                          )),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                  width: 100,
+                                  child: const Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      Icon(
+                                        Icons.thumb_up_outlined,
+                                        size: 30,
+                                      ),
+                                      Text("Suka")
+                                    ],
+                                  )),
+                              Container(
+                                  width: 100,
+                                  child: const Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      Icon(
+                                        Icons.comment_outlined,
+                                        size: 30,
+                                      ),
+                                      Text("Komentar")
+                                    ],
+                                  )),
+                              Container(
+                                  width: 100,
+                                  child: const Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      Icon(
+                                        Icons.share,
+                                        size: 30,
+                                      ),
+                                      Text("Bagikan")
+                                    ],
+                                  )),
+                            ],
+                          ),
+                        )
                       ],
                     ),
                   )
